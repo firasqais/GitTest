251d7d1f05f4bc07265f15903157ba6e694ec516
firasqais <30744313+firasqais@users.noreply.github.com>
2023-11-17 11:25:27 +1100

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Firas on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
